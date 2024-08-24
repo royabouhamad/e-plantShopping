21726c330ceaa0cd4eccc6aba2b20867bbafc59f
Roy Abouhamad <roy.d.abouhamad@gmail.com>
2024-08-24 14:27:40 -0400

    Show total number of items in cart

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,31 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-        const {name, image, cost} = action.payload;
-        const existingItem = state.items.find(item => item.name === name);
-        if (existingItem) {
-            existingItem.quantity++;
-        } else {
-            state.items.push({ name, image, cost, quantity: 1});
-        }
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
+        totalQuantity: 0,
     },
-    removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload); 
-    },
-    updateQuantity: (state, action) => {
-        const {name, quantity} = action.payload;
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if (itemToUpdate && quantity > 0) {
-            itemToUpdate.quantity = quantity;
-        }
-    },
-  },
+    reducers: {
+        addItem: (state, action) => {
+            const { name, image, cost } = action.payload;
+            const existingItem = state.items.find(item => item.name === name);
+            if (existingItem) {
+                existingItem.quantity++;
+            } else {
+                state.items.push({ name, image, cost, quantity: 1 });
+            }
+            state.totalQuantity++;
+        },
+        removeItem: (state, action) => {
+            const currentItem = state.items.find(item => item.name === action.payload);
+            if (currentItem) {
+                state.totalQuantity -= currentItem.quantity;
+            }
+            state.items = state.items.filter(item => item.name !== action.payload);
+        },
+        updateQuantity: (state, action) => {
+            const { name, quantity } = action.payload;
+            const itemToUpdate = state.items.find(item => item.name === name);
+            if (itemToUpdate && quantity > 0) {
+                state.totalQuantity = state.totalQuantity - itemToUpdate.quantity + quantity;
+                itemToUpdate.quantity = quantity;
+            }
+        },
+    }
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
